aba79dc14acf6643a3ed0ba9c12efd677a37f8bd
abdul <abdoolihima@gmail.com>
2025-04-22 23:18:45 +0100

    Add more queries for auth

--- query.sql
+++ query.sql
@@ -8,4 +8,27 @@ FROM users u INNER JOIN auth a
 ON u.user_id = a.user_id
 WHERE u.user_id = ? LIMIT 1;
 
--- update
+-- name: CreateUser :execresult
+INSERT INTO users (
+  email, password
+) VALUES (
+  ?, ?
+);
+
+-- name: UpdateUser :execresult
+UPDATE users 
+SET email = ?, password = ?
+WHERE user_id = ?;
+
+-- name: UpdateUserAuth :execresult
+UPDATE auth 
+SET session_token= ?, password_hash = ?, csrf_token = ?
+WHERE auth_id = ? OR user_id = ?;
+
+-- name: DeleteUserAuth :exec
+DELETE FROM auth
+WHERE auth_id = ? OR user_id = ?;
+
+-- name: DeleteUser :exec
+DELETE FROM users
+WHERE user_id = ?;
