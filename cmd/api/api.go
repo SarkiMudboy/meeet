83d1b6b76af3c29f5f451e4254ce65470582c79c
abdul <abdoolihima@gmail.com>
2025-04-30 22:41:33 +0100

    refactor: migrating to repo pattern: initial project setup

---
+++ cmd/api/api.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (a *application) mount() http.Handler {
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Route("v1/auth", func(r chi.Router) {
+		r.Post("/register", a.healthCheck)
+		r.Post("/login", a.healthCheck)
+		r.Post("/logout", a.healthCheck)
+		r.Post("/create-meeting", a.healthCheck)
+	})
+
+	r.Get("/health", a.healthCheck)
+	return r
+}
+
+func (a *application) run(mux http.Handler) error {
+
+	server := &http.Server{
+		Addr:         a.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 10,
+		ReadTimeout:  time.Second * 8,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("server has started at %s\n", a.config.addr)
+	if err := server.ListenAndServe(); err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/golang-migrate/migrate/v4 v4.18.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
